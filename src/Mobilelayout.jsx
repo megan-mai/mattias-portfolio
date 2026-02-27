e4548da6a58fe60e25aa0772ac43870d5aa2fa1e
Megan <mpmai99@gmail.com>
2026-02-27 16:01:49 -0500

    centered div

--- src/Mobilelayout.jsx
+++ src/Mobilelayout.jsx
@@ -6,7 +6,7 @@ function MobileLayout({ selectedYear, setSelectedYear, showPrompt, setShowPrompt
     const tickPosition = ((2026 - selectedYear) / (2026 - 2015)) * 100
     const roles = ["Software Engineer", "Game Dev"]
   return (
-     <div className="h-screen m-0 flex flex-col items-center justify-center">
+<div className="flex flex-col items-center justify-center overflow-hidden" style={{ height: '100dvh' }}>
       <div className="flex w-fit m-auto gap-[2em]">
          <div className="relative flex flex-col h-full">
           <div className="flex-[25%] border-r-1 border-solid"></div>
@@ -63,7 +63,7 @@ function MobileLayout({ selectedYear, setSelectedYear, showPrompt, setShowPrompt
         })}
           </div>
 
-          <a href="mailto:mattiasl380@gmail.com" className="flex flew-row items-center cursor-pointer fade-in hover:underline" style={{ animationDelay: '600ms'}}>
+          <a href="mailto:mattiasl380@gmail.com" className="flex flew-row items-center cursor-pointer fade-in hover:underline" style={{ animationDelay: '300ms'}}>
             mattiasl380@gmail.com 
             <svg className='mx-[2px] mt-[2px]' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" viewBox="0 0 256 256"><path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"></path></svg>
           </a>
